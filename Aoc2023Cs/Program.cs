75f07b3fff2a9dba2282930cdad2a55291dfb0af
tobiesque <tobias@sicheritz.net>
2023-12-06 20:22:09 +0100

    Day 5 with inlined data

--- Aoc2023Cs/Program.cs
+++ Aoc2023Cs/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Aoc2023Cs;
 
-
 // Day1.Run();
 //Day2one.Run();
 // Day2two.Run();
 // Day3.Run();
 //Day4.Run();
 //Day5.Run();
+Day5inline.Run();
 //Day6one.Run();
-Day6two.Run();
+//Day6two.Run();

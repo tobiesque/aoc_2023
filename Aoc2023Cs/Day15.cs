090d7775d1b7773b2c98d4dcd38e96c5a9a155f3
tobiesque <tobias@sicheritz.net>
2023-12-20 02:50:26 +0100

    Day 15 Part Two

--- Aoc2023Cs/Day15.cs
+++ Aoc2023Cs/Day15.cs
@@ -1,4 +1,6 @@
-﻿namespace Aoc2023Cs;
+﻿using Rock.Collections;
+
+namespace Aoc2023Cs;
 
 public class Day15
 {
@@ -9,31 +11,51 @@ public class Day15
         partOne = (part == 1);
         string[] entries = File.ReadAllText("15".GetInputFile(test: false)).Split(',');
 
-        if (partOne)
-        {
-            PartOne(entries);
-            return;
-        }
+        if (partOne) PartOne(entries); else PartTwo(entries);
+    }
 
-        foreach (var entry in entries)
+    public static void PartTwo(string[] entries)
+    {
+        Dictionary<long, Box> boxes = new ();
+
+        Box.Dump(boxes.Values);
+        
+        foreach (string entry in entries)
         {
+            Console.WriteLine(entry);
             if (entry.Contains('-'))
             {
-                long label = Hash(entry[..^1]);
-            } else if (entry.Contains('='))
-            {
+                string label = entry[..^1];
+                long boxNumber = Hash(label);
+                if (boxes.TryGetValue(boxNumber, out var box))
+                {
+                    box.Remove(label);
+                }
             }
+            else if (entry.Contains('='))
+            {
+                var values = entry.Split('=');
+                string label = values[0];
+                long boxNumber = Hash(label);
+                long focal = long.Parse(values[1]);
+
+                if (!boxes.TryGetValue(boxNumber, out var box))
+                {
+                    box = new Box(boxNumber);
+                    boxes[boxNumber] = box;
+                }
+
+                box.Add(label, focal);
+            }
+            
         }
+        Box.Dump(boxes.Values);
+        Console.WriteLine($"Part Two: {Box.Score(boxes.Values)}");
     }
     
     public static void PartOne(string[] entries)
     {
-        long result = 0;
-        foreach (string entry in entries)
-        {
-            result += Hash(entry);
-        }
-        Console.WriteLine($"Part One: {result}");
+        Console.WriteLine($"Part One: {entries.Sum(Hash)}");
     }
     
     public static long Hash(string s)
@@ -46,5 +68,51 @@ public class Day15
             value %= 256;
         }
         return value;
+    }
+
+    public class Box
+    {
+        public long id;
+
+        public readonly OrderedDictionary<string, long> lenses = new ();
+
+        public void Add(string label, long focal) => lenses[label] = focal;
+        public void Remove(string label) => lenses.Remove(label);
+
+        public Box(long id) => this.id = id;
+
+        public long Score()
+        {
+            long score = 0;
+            int i = 1;
+            foreach (var (_, lens) in lenses)
+            {
+                long result = id + 1;
+                result *= i;
+                result *= lens;
+                score += result;
+                ++i;
+            }
+            return score;
+        }
+
+        public static long Score(IEnumerable<Box> boxes) => boxes.Sum(box => box.Score());
+
+        public override string ToString()
+        {
+            if (lenses.Count == 0) return "";
+            string lensesList = lenses.MakeList("][");
+            return $"Box {id}: {lensesList}";
+        }
+
+        public static void Dump(IEnumerable<Box> boxes)
+        {
+            foreach (var box in boxes)
+            {
+                string boxPrint = box.ToString();
+                if (string.IsNullOrWhiteSpace(boxPrint)) continue;
+                Console.WriteLine(boxPrint);
+            }
+        }
     }
 }
